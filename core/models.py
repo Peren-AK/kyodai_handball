4a749db4eb4920460a8b41feb1016af3a8ea106d
Peren-AK <hayaaki.gfx@gmail.com>
2026-02-25 20:43:35 +0900

    フッター・ヘッダーおよび軽微な修正

--- core/models.py
+++ core/models.py
@@ -6,6 +6,7 @@ class SiteSettings(BaseSiteSetting):
     ga4_measurement_id = models.CharField(max_length=50, blank=True, help_text="例: G-XXXXXXXXXX")
     contact_form_url = models.URLField(blank=True, help_text="GoogleフォームのURL")
     sponsor_form_url = models.URLField(blank=True, help_text="スポンサー用GoogleフォームのURL")
+    youtube_url = models.URLField(blank=True)
     instagram_url = models.URLField(blank=True)
     x_url = models.URLField(blank=True)
 

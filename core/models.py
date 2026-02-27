7dd31423cc586fe0e780727075d06db57ce11dc3
Peren-AK <hayaaki.gfx@gmail.com>
2026-02-27 11:02:05 +0900

    スポンサー紹介

--- core/models.py
+++ core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from wagtail.contrib.settings.models import BaseSiteSetting, register_setting
+from wagtail.snippets.models import register_snippet
+from wagtail.admin.panels import FieldPanel
 
 @register_setting
 class SiteSettings(BaseSiteSetting):
@@ -12,3 +14,35 @@ class SiteSettings(BaseSiteSetting):
 
     class Meta:
         verbose_name = "サイト全体設定"
+
+# ここからスポンサー用のデータベース
+@register_snippet
+class Sponsor(models.Model):
+    name = models.CharField(max_length=100, verbose_name="スポンサー名")
+    logo = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        verbose_name="ロゴ画像"
+    )
+    url = models.URLField(blank=True, verbose_name="リンク先URL", help_text="クリック時の飛び先")
+    display_order = models.IntegerField(default=0, verbose_name="表示順", help_text="数字が小さいほど左に表示されます")
+    is_published = models.BooleanField(default=True, verbose_name="公開する")
+
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('logo'),
+        FieldPanel('url'),
+        FieldPanel('display_order'),
+        FieldPanel('is_published'),
+    ]
+
+    class Meta:
+        ordering = ['display_order', 'id']
+        verbose_name = "スポンサー"
+        verbose_name_plural = "スポンサー一覧"
+
+    def __str__(self):
+        return self.name
